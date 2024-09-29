68716f768d619904988bc628619425c5f94e0bae
tiennm2000 <tien.nm.2409@aptechlearning.edu.vn>
2024-09-29 19:17:49 +0700

    config image url

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*flaticon.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
